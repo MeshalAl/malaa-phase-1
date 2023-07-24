2f49ac310e5acff9e749e904904575369ec5466f
MeshalAl <meshalalanazinsa@gmail.com>
2023-07-24 15:38:49 +0300

    phase 1 initial

---
+++ phase_1/investor_bulletin/resources/alert_rules/alert_rule_schema.py
@@ -0,0 +1,8 @@
+""" Alert Rule Schema """
+"""_summary_
+This file to abstract any validation logic for the Alert Rules
+"""
+from pydantic import BaseModel
+
+class AlertRuleCreate(BaseModel):
+    pass
