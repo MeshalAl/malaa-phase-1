63f53e934b7e65c3ce65ad569e4f2f561da953e0
Meshal <meshalalanazinsa@gmail.com>
2023-08-01 21:52:04 +0300

    added env loading

--- phase_1/investor_bulletin/api/main.py
+++ phase_1/investor_bulletin/api/main.py
@@ -1,5 +1,4 @@
-import os
-import sys
+import os, sys
 from uvicorn import run
 from fastapi import FastAPI
 
@@ -7,8 +6,10 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 root_dir = os.path.dirname(current_dir)
 sys.path.append(root_dir)
 
-from routes import init_routes
+from api.routes import init_routes
 app = init_routes(FastAPI())
 
 if __name__ == "__main__":
+    from _config.config import load_env
+    load_env()
     run("api.main:app")
