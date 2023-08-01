c8c931ef7d00acb3723a11055762945b89da4922
Meshal <meshalalanazinsa@gmail.com>
2023-08-01 21:53:37 +0300

    added rules route

--- phase_1/investor_bulletin/api/routes.py
+++ phase_1/investor_bulletin/api/routes.py
@@ -3,4 +3,5 @@ from api.controllers.rules_controller import router as RulesRouter
 
 def init_routes(app):
     app.include_router(MarketRouter, prefix="/market-prices", tags=["Market"])
+    app.include_router(RulesRouter, prefix="/alerts", tags=["Alerts"])
     return app
