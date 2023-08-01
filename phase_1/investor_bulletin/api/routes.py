7432cb21f45f983ea94e030c6ba9edfc94c6b6dd
MeshalAl <meshal@alanazi.io>
2023-08-01 12:23:22 +0300

    added rule route import

--- phase_1/investor_bulletin/api/routes.py
+++ phase_1/investor_bulletin/api/routes.py
@@ -1,4 +1,5 @@
 from api.controllers.market_controllers import router as MarketRouter
+from api.controllers.rules_controller import router as RulesRouter
 
 def init_routes(app):
     app.include_router(MarketRouter, prefix="/market-prices", tags=["Market"])
