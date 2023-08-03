fbec12c9e656f5207acfd5b1c05c5c00bd96d8c4
MeshalAl <meshal@alanazi.io>
2023-08-03 17:09:44 +0300

    reorganized structure

--- phase_1/investor_bulletin/db/models/model_base.py
+++ phase_1/investor_bulletin/db/models/model_base.py
@@ -1,7 +1,4 @@
-import os
-from sqlalchemy import create_engine
+
 from sqlalchemy.ext.declarative import declarative_base
 
-db_url = os.environ.get('DATABASE_URL')
-engine = create_engine(db_url, echo=True)
 Base = declarative_base()
