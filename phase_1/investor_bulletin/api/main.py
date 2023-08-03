710cfc726238641c84ea487adcd60a0d6dfe1042
MeshalAl <meshal@alanazi.io>
2023-08-03 17:08:55 +0300

    fixed env loading prior to local imports

--- phase_1/investor_bulletin/api/main.py
+++ phase_1/investor_bulletin/api/main.py
@@ -6,10 +6,11 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 root_dir = os.path.dirname(current_dir)
 sys.path.append(root_dir)
 
+from _config.config import load_env
+load_env()
+
 from api.routes import init_routes
 app = init_routes(FastAPI())
 
 if __name__ == "__main__":
-    from _config.config import load_env
-    load_env()
     run("api.main:app")
