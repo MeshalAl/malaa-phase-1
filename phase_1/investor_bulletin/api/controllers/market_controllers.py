ddb71fc92589d79b1efda35b0c7bc3d0dfeb9cef
MeshalAl <meshal@alanazi.io>
2023-08-06 18:56:11 +0300

    added reponse_model to route

--- phase_1/investor_bulletin/api/controllers/market_controllers.py
+++ phase_1/investor_bulletin/api/controllers/market_controllers.py
@@ -4,6 +4,6 @@ from resources.market.market_service import get_market_data
 
 router = APIRouter()
 
-@router.get('/') #response_model=MarketPriceResponse
+@router.get('/', response_model=MarketPriceResponse)
 def get_market_data_route():
     return get_market_data()
