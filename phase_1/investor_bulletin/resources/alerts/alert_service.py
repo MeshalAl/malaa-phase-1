b388b26424904a6b50994264603a7136e393de19
MeshalAl <meshal@alanazi.io>
2023-08-06 18:44:14 +0300

    added CRUD

--- phase_1/investor_bulletin/resources/alerts/alert_service.py
+++ phase_1/investor_bulletin/resources/alerts/alert_service.py
@@ -5,8 +5,8 @@ this file to write any business logic for the Rules
 from resources.alerts.alert_schema import AlertCreate
 from resources.alerts.alert_dal import create_alert, get_alerts
 
-def create_new_alert_service( rule: AlertCreate, session ):
-    return create_alert( rule=rule, session=session)
+def create_new_alert_service( alert: AlertCreate, session ):
+    return create_alert( alert=alert, session=session)
 
 def get_alerts_service(session):
     return get_alerts(session)
